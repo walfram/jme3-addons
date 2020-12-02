e5373dd0cc090850fe34a3824148b0823780978d
jevgeni <jevgeni.kalinin@gmail.com>
2020-12-02 11:29:01 +0200

    added 'fast' point-mode mesh

---
+++ src/main/java/jme3/common/mesh/PointMesh.java
@@ -0,0 +1,19 @@
+package jme3.common.mesh;
+
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.VertexBuffer.Type;
+import com.jme3.util.BufferUtils;
+
+public final class PointMesh extends Mesh {
+
+	public PointMesh(Vector3f[] vertices) {
+		setMode(Mode.Points);
+
+		setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
+
+		updateCounts();
+		updateBound();
+	}
+
+}
