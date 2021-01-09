1d2d0b9513bf28b6b357c6edd56d3658b67cd8bd
jevgeni <jevgeni.kalinin@gmail.com>
2021-01-09 13:03:04 +0200

    added secondary constructor to PointMesh

--- src/main/java/jme3/common/mesh/PointMesh.java
+++ src/main/java/jme3/common/mesh/PointMesh.java
@@ -1,11 +1,17 @@
 package jme3.common.mesh;
 
+import java.util.List;
+
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.VertexBuffer.Type;
 import com.jme3.util.BufferUtils;
 
 public final class PointMesh extends Mesh {
+
+	public PointMesh(List<Vector3f> vertices) {
+		this(vertices.stream().toArray(Vector3f[]::new));
+	}
 
 	public PointMesh(Vector3f[] vertices) {
 		setMode(Mode.Points);
