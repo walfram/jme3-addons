6ccb09e92787f2dfaf0bdcdc4c348c7f0b04c8c5
jevgeni <jevgeni.kalinin@gmail.com>
2021-02-26 20:53:43 +0200

    added json export to noise settings

--- src/main/java/jme3/common/noise/NoiseSettings.java
+++ src/main/java/jme3/common/noise/NoiseSettings.java
@@ -1,5 +1,7 @@
 package jme3.common.noise;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import jme3.common.noise.FastNoiseLite.CellularDistanceFunction;
 import jme3.common.noise.FastNoiseLite.CellularReturnType;
 import jme3.common.noise.FastNoiseLite.DomainWarpType;
@@ -10,15 +12,17 @@ import jme3.common.noise.FastNoiseLite.RotationType3D;
 public class NoiseSettings {
 
 	public NoiseType mNoiseType = NoiseType.OpenSimplex2;
-	public RotationType3D mRotationType3D = RotationType3D.None;
 	public FractalType mFractalType = FractalType.None;
 
 	public float mFrequency = 0.01f;
 	public int mOctaves = 3;
 	public float mLacunarity = 2.0f;
 	public float mGain = 0.5f;
+
 	public float mWeightedStrength = 0.0f;
 	public float mPingPongStength = 2.0f;
+
+	public RotationType3D mRotationType3D = RotationType3D.None;
 
 	public CellularDistanceFunction mCellularDistanceFunction = CellularDistanceFunction.EuclideanSq;
 	public CellularReturnType mCellularReturnType = CellularReturnType.Distance;
@@ -45,6 +49,23 @@ public class NoiseSettings {
 
 		noise.SetDomainWarpType(mDomainWarpType);
 		noise.SetDomainWarpAmp(mDomainWarpAmp);
+	}
+
+	public void toJson(ObjectNode node) {
+		node.put("mNoiseType", mNoiseType.name());
+		node.put("mFractalType", mFractalType.name());
+		node.put("mFrequency", mFrequency);
+		node.put("mOctaves", mOctaves);
+		node.put("mLacunarity", mLacunarity);
+		node.put("mGain", mGain);
+		node.put("mWeightedStrength", mWeightedStrength);
+		node.put("mPingPongStength", mPingPongStength);
+		node.put("mRotationType3D", mRotationType3D.name());
+		node.put("mCellularDistanceFunction", mCellularDistanceFunction.name());
+		node.put("mCellularReturnType", mCellularReturnType.name());
+		node.put("mCellularJitterModifier", mCellularJitterModifier);
+		node.put("mDomainWarpType", mDomainWarpType.name());
+		node.put("mDomainWarpAmp", mDomainWarpAmp);
 	}
 
 }
